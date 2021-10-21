0c6c4d62acdd6dd0d348b5a0cc321d06df95e87d
26ANSH <anshvidyabhanu8@gmail.com>
2021-10-21 17:20:03 +0530

    fixed bugs

--- main.py
+++ main.py
@@ -24,5 +24,4 @@ def camera():
              
 
 if __name__ == '__main__':
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'qwiklabs-gcp-02-e6d2430310bc-faf00e53dd37.json'
     app.run(host='127.0.0.1', port=8080, debug=True)
